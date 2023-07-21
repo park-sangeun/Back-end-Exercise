d30ff9a79ee64e35182520fa23c36976585cc5bf
Park Sangeun <90459890+park-sangeun@users.noreply.github.com>
2023-07-21 15:41:56 +0900

    Add files via upload

---
+++ 3_Login/4_ID Login&Logout/idLogin/idLogin/src/main/java/com/idLogin/idLogin/entity/MemberEntity.java
@@ -0,0 +1,44 @@
+package com.idLogin.idLogin.entity;
+
+import com.idLogin.idLogin.dto.MemberDTO;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Setter
+@Getter
+@Table(name = "data_ex")
+public class MemberEntity {
+    @Id // pk 지정
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment
+    private Long user_no;
+
+    @Column(name = "userId", unique = true)
+    private String userId;
+
+    @Column
+    private String user_password;
+
+    @Column
+    private String name;
+
+    public static MemberEntity toMemberEntity(MemberDTO memberDTO) {
+        MemberEntity memberEntity = new MemberEntity();
+        memberEntity.setUserId(memberDTO.getUserId());
+        memberEntity.setUser_password(memberDTO.getUser_password());
+        memberEntity.setName(memberDTO.getName());
+        return memberEntity;
+    }
+
+    public static MemberEntity toUpdateMemberEntity(MemberDTO memberDTO) {
+        MemberEntity memberEntity = new MemberEntity();
+        memberEntity.setUser_no(memberDTO.getUser_no());
+        memberEntity.setUserId(memberDTO.getUserId());
+        memberEntity.setUser_password(memberDTO.getUser_password());
+        memberEntity.setName(memberDTO.getName());
+        return memberEntity;
+    }
+
+}
